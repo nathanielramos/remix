linket-protocol/remix-project

move classes from remix-lib to remix-debug

libs/remix-debug/src/trace/traceHelper.js
@@ -1,5 +1,6 @@
 'use strict'
-const ui = require('./uiHelper')
+const remixLib = require('@remix-project/remix-lib')
+const ui = remixLib.helper.ui
 
 module.exports = {
   // vmTraceIndex has to point to a CALL, CODECALL, ...

nathanielramos/remix

libs/remix-debug/src/trace/traceHelper.js
@@ -1,5 +1,6 @@
 'use strict'
-const ui = require('./uiHelper')
+const remixLib = require('@remix-project/remix-lib')
+const ui = remixLib.helpers.ui
 
 module.exports = {
   // vmTraceIndex has to point to a CALL, CODECALL, ...